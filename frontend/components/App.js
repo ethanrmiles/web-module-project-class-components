a222b5f5f409f189c245f095d5085f9b410af490
Ethan Miles <ethanmilescs@gmail.com>
2022-03-07 14:03:08 -0600

    setup initial states

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
+let id = 0
+const getId = () => id++;
+
+const initialState = {
+   id: getId() ,
+   name: '',
+   completed: false
+}
+
+const toDos = 
+[
+  {
+    name: 'Organize Garage',
+    id: 1,
+    completed: false
+  },
+
+  {
+    name: 'Bake Cookies',
+    id: 2,
+    completed: false
+  }
+]
+
+
 export default class App extends React.Component {
+state = initialState
+
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todos:</h1>
       </div>
     )
   }
